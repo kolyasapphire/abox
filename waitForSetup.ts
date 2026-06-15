641f5c6ce9a1c4a8e35a8a937f722899b41ed935
kolyasapphire <a@ks.gg>
2026-06-15 03:33:42 +0100

    Proper setup completion checks

--- waitForSetup.ts
+++ waitForSetup.ts
@@ -3,10 +3,15 @@ import { wait } from './utils.ts'
 
 export const waitForSetup = async (ip: string) => {
   while (true) {
-    const ssh = await executeSsh(ip, [
-      'cloud-init status --wait --format json',
-    ])
-    if (ssh.code === 0) break
+    const { stdout: status } = await executeSsh(ip, ['cloud-init status'])
+
+    if (status.includes('status: error')) {
+      const err = await executeSsh(ip, ['cloud-init status --long'])
+      throw new Error(`Setup failed: ${err.stdout}`)
+    }
+
+    if (status.includes('status: done')) break
+
     await wait()
   }
 }
