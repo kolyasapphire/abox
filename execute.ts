d7053f4616bdbae4416862fd10eae624aea0f66e
kolyasapphire <a@ks.gg>
2026-06-15 03:23:06 +0100

    Rework executors

--- execute.ts
+++ execute.ts
@@ -1,11 +1,27 @@
+const withStdDecoded = (res: Deno.CommandOutput) => ({
+  ...res,
+  stdout: new TextDecoder().decode(res.stdout),
+  stderr: new TextDecoder().decode(res.stderr),
+})
+
 export const execute = async ([cmd, ...args]: string[]) => {
-  const res = await new Deno.Command(cmd, { args: args }).output()
-
+  const res = await Deno.spawnAndWait(cmd, args)
   if (!res.success) throw new Error(`Command failed with exit code ${res.code}`)
-
-  return res
+  return withStdDecoded(res)
 }
 
-export const executeSsh = async (ip: string, cmd: string[]) =>
-  await new Deno.Command('ssh', { args: ['-o', 'StrictHostKeyChecking=no', `root@${ip}`, ...cmd] })
-    .output()
+const skipHostKeyChecking = ['-o', 'StrictHostKeyChecking=no']
+
+export const executeSsh = async (ip: string, cmd: string[]) => {
+  const res = await Deno.spawnAndWait('ssh', [...skipHostKeyChecking, `root@${ip}`, ...cmd])
+  return withStdDecoded(res)
+}
+
+export const executeSshStreamed = async (ip: string, cmd: string[]) => {
+  const { stdout } = Deno.spawn(
+    'ssh',
+    [...skipHostKeyChecking, `root@${ip}`, ...cmd],
+    { stdout: 'piped' },
+  )
+  for await (const chunk of stdout) process.stdout.write(new TextDecoder().decode(chunk))
+}
